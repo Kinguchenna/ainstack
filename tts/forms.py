2c2d99190df6ee7aaa1d472c66dad9e39c7b5329
Kinguchenna <basiluchenna@gmail.com>
2025-07-04 17:58:08 +0100

    Delete tts directory

--- tts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import CustomVoice
-
-class CustomVoiceForm(forms.ModelForm):
-    class Meta:
-        model = CustomVoice
-        fields = ['voice_name', 'voice_file']
